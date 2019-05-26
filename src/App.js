43debd8a30b02219e05a497034776f3e3c81a421
Mike <michael.barbone@gmail.com>
2019-05-25 21:50:43 -0400

    Creation of hero and rename of files.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
-import SideNavBar from "./components/sideNavBar/sideNavbar";
+import SideNavBar from "./components/sideNavBar/index";
+import Hero from "./components/hero/index";
 
 function App() {
   return (
     <div className="App">
       <SideNavBar />
+      <Hero />
     </div>
   );
 }
