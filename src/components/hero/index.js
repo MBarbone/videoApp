aecb08f35d36a3ed2704b9d858fd39270cb4a9e2
Mike <michael.barbone@gmail.com>
2019-05-29 17:21:40 -0400

    Solid draft for hero. Slider testing

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -3,8 +3,18 @@ import "./style.css";
 
 function Hero(props) {
   return (
-    <div className="hero text-center">
-      <h1>Hey This is a feature</h1>
+    <div className="hero">
+      <div className="hero-btn-container">
+        <h4>Watch Now!</h4>
+        <button className="btn btn-secondary hero-btn">
+          <i className="fas fa-info-circle" />
+          More Info
+        </button>
+        <button className="btn btn-secondary hero-btn">
+          <i className="fas fa-plus" />
+          Add to Favorites
+        </button>
+      </div>
     </div>
   );
 }
