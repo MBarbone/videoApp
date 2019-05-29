5b9b8c80360e656726481f65268d928f81b35a1e
Mike <michael.barbone@gmail.com>
2019-05-29 17:36:12 -0400

    Resize item images, clean up css code, create slider section title template, adjust slider container width

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -3,12 +3,15 @@ import "./style.css";
 
 function Slider() {
   return (
-    <div className="container slider-container">
-      <div className="item">1</div>
-      <div className="item">2</div>
-      <div className="item">3</div>
-      <div className="item">4</div>
-      <div className="item">5</div>
+    <div>
+      <h3 className="section-title">Title of Section</h3>
+      <div className="slider-item-container">
+        <div className="item">1</div>
+        <div className="item">2</div>
+        <div className="item">3</div>
+        <div className="item">4</div>
+        <div className="item">5</div>
+      </div>
     </div>
   );
 }
