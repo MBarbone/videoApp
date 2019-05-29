a8d5fe91ea8a3558a2140956ebba21bbb632f9d8
Mike <michael.barbone@gmail.com>
2019-05-28 22:59:56 -0400

    Hero redesign and clean up CSS code.

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -3,16 +3,8 @@ import "./style.css";
 
 function Hero(props) {
   return (
-    <div className="container hero-container">
-      <div className="row">
-        <div className="col-1" />
-        <div className="col-10">
-          <div className="hero text-center">
-            <h1>Hey This is a feature</h1>
-          </div>
-        </div>
-        <div className="col-1" />
-      </div>
+    <div className="hero text-center">
+      <h1>Hey This is a feature</h1>
     </div>
   );
 }
