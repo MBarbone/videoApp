b9f6c096a42dc7fb1b2a4ed87093f9f6e05977db
Mike <michael.barbone@gmail.com>
2019-05-28 22:47:22 -0400

    Rename nav bar, move nav bar to top instead of left side. Redesign app layout

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -3,15 +3,13 @@ import "./style.css";
 
 function NavBar() {
   return (
-    <div className="nav-container">
+    <div className="nav">
       <i className="fas fa-play fa-4x" />
-      <div className="sidenav">
-        <a href="#home">Home</a>
-        <a href="#search">Search</a>
-        <a href="#notifications">Your Favorites</a>
-        <a href="#settings">Settings</a>
-        <a href="#signOut">Sign Out</a>
-      </div>
+      <a href="#home">Home</a>
+      <a href="#search">Search</a>
+      <a href="#favorites">Favorites</a>
+      <a href="#settings">Settings</a>
+      <a href="#signOut">Sign Out</a>
     </div>
   );
 }
