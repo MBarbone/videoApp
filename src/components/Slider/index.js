a3f56610976837ce3b2e6423c05afdb79cbbbb28
Mike <michael.barbone@gmail.com>
2019-06-05 23:24:21 -0400

    Bring in axios for hitting movie db api. Refactor slider to a class based component to be able to handle state change without use of hook

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,19 +1,53 @@
 import React from "react";
+import API from "../../utils/MovieDiscoverAPI";
 import "./style.css";
 
-function Slider() {
-  return (
-    <div>
-      <h3 className="section-title">Title of Section</h3>
-      <div className="slider-item-container">
-        <div className="item">1</div>
-        <div className="item">2</div>
-        <div className="item">3</div>
-        <div className="item">4</div>
-        <div className="item">5</div>
+// function Slider() {
+//   return (
+//     <div>
+//       <h3 className="section-title">Title of Section</h3>
+//       <div className="slider-item-container">
+//         <div className="item">1</div>
+//         <div className="item">2</div>
+//         <div className="item">3</div>
+//         <div className="item">4</div>
+//         <div className="item">5</div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Slider;
+
+class Slider extends React.Component {
+  state = {
+    result: {}
+  };
+
+  componentDidMount() {
+    this.discoverMovies();
+  }
+
+  discoverMovies = query => {
+    API.discoverMovies()
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+      <div>
+        <h3 className="section-title">Title of Section</h3>
+        <div className="slider-item-container">
+          <div className="item">1</div>
+          <div className="item">2</div>
+          <div className="item">3</div>
+          <div className="item">4</div>
+          <div className="item">5</div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Slider;
